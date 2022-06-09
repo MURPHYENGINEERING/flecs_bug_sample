8979284d9bc19a750c9b9dfe6264818e116620f4
Matt Murphy <matt@murphysys.com>
2022-06-09 12:09:41 -0700

    clarify solution

--- common.c
+++ common.c
@@ -8,7 +8,7 @@
     ecs_entity_desc_t desc = {0};         \
     desc.entity = id;                     \
     desc.name = #id;                      \
-    desc.symbol = #id;                    \
+    desc.symbol = #id;                    \     // <------ add this
     desc.add_expr = #__VA_ARGS__;         \
     id = ecs_entity_init(world, &desc);   \
     ecs_id(id) = id;                      \
@@ -18,11 +18,12 @@
 
 #endif
 
+
 ECS_DECLARE(SharedTag);
 
 void CommonImport(ecs_world_t *world)
 {
   ECS_MODULE(world, Common);
-  
+
   ECS_TAG_DEFINE(world, SharedTag);
 }
